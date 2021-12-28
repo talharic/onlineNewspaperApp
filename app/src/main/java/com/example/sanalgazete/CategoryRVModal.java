34ca835deaa6ddb90842e9b41cc73702dc9e9fa0
Talha Arıç <talharic@gmail.com>
2021-12-28 20:21:05 +0300

    fix some ui related issues

--- app/src/main/java/com/example/sanalgazete/CategoryRVModal.java
+++ app/src/main/java/com/example/sanalgazete/CategoryRVModal.java
@@ -2,12 +2,16 @@ package com.example.sanalgazete;
 
 public class CategoryRVModal {
     private String category;
+    private String apiName;
     private String categoryImageUrl;
 
     public String getCategory() {
         return category;
     }
 
+    public String getApiName() {
+        return this.apiName;
+    }
     public void setCategory(String category) {
         this.category = category;
     }
@@ -21,7 +25,30 @@ public class CategoryRVModal {
     }
 
     public CategoryRVModal(String category, String categoryImageUrl) {
-        this.category = category;
+        switch (category){
+            case "All":
+                this.category = "Tümü";
+                break;
+            case "Technology":
+                this.category = "Teknoloji";
+                break;
+            case "Science":
+                this.category = "Bilim";
+                break;
+            case "Sports":
+                this.category = "Spor";
+                break;
+            case "Business":
+                this.category = "İş";
+                break;
+            case "Entertainment":
+                this.category = "Eğlence";
+                break;
+            case "Health":
+                this.category = "Sağlık";
+                break;
+        }
+        this.apiName = category;
         this.categoryImageUrl = categoryImageUrl;
     }
 }
--- app/src/main/java/com/example/sanalgazete/MainActivity.java
+++ app/src/main/java/com/example/sanalgazete/MainActivity.java
@@ -19,7 +19,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity implements CategoryRVAdapter.CategoryClickInterface{
 
-    //5042b6d940884bcc9b107837bba7aaf3 (News Api bilgim)
+    //5042b6d940884bcc9b107837bba7aaf3
 
     private RecyclerView newsRV,categoryRV;
     private ProgressBar loadingPB;
@@ -49,13 +49,13 @@ public class MainActivity extends AppCompatActivity implements CategoryRVAdapter
     }
 
     private void getCategories(){
-        categoryRVModalArrayList.add(new CategoryRVModal("All", "https://media.istockphoto.com/photos/newspapers-picture-id513051742?b=1&k=20&m=513051742&s=170667a&w=0&h=ax1sVBU4REigOoOp3c28CntSSDKvg-eHIiSzOIPV6sM="));
-        categoryRVModalArrayList.add(new CategoryRVModal("Technology", "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaG5vbG9neXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"));
-        categoryRVModalArrayList.add(new CategoryRVModal("Science", "https://media.istockphoto.com/photos/asian-female-doctor-working-with-pathogen-samples-using-microscope-picture-id1256323051?b=1&k=20&m=1256323051&s=170667a&w=0&h=mrZs1Fzc0yjnus3l0WALeqTgE7SWhdPpBZ4C6gTQ2Wg="));
-        categoryRVModalArrayList.add(new CategoryRVModal("Sport", "https://media.istockphoto.com/photos/various-sport-equipments-on-grass-picture-id949190736?b=1&k=20&m=949190736&s=170667a&w=0&h=f3ofVqhbmg2XSVOa3dqmvGtHc4VLA_rtbboRGaC8eNo="));
-        categoryRVModalArrayList.add(new CategoryRVModal("Business", "https://images.unsplash.com/39/lIZrwvbeRuuzqOoWJUEn_Photoaday_CSD%20(1%20of%201)-5.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVzaW5lc3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"));
-        categoryRVModalArrayList.add(new CategoryRVModal("Economy", "https://media.istockphoto.com/photos/business-document-report-on-paper-and-tablet-with-sales-data-and-picture-id1182777182?b=1&k=20&m=1182777182&s=170667a&w=0&h=Bdq_sXQIFf64FIoPQQsTuBt1-FnkbtVcm96LR7P_0Pc="));
-        categoryRVModalArrayList.add(new CategoryRVModal("Health", "https://images.unsplash.com/photo-1526256262350-7da7584cf5eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGhlYWx0aHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"));
+        categoryRVModalArrayList.add(new CategoryRVModal("All", "https://drive.google.com/uc?export=view&id=1_Y91OHWGApZbxroFkrijV55_vDLS4nR_"));
+        categoryRVModalArrayList.add(new CategoryRVModal("Technology", "https://drive.google.com/uc?export=view&id=1OpbRKp2gXptScxmC9efyZrz_wpl7BrjC"));
+        categoryRVModalArrayList.add(new CategoryRVModal("Science", "https://drive.google.com/uc?export=view&id=1dZVSAgtWkBpGa_pqvHjcN3kmBGCHnvfz"));
+        categoryRVModalArrayList.add(new CategoryRVModal("Sports", "https://drive.google.com/uc?export=view&id=1z0lES2fui4QaHGf6X1jeP7bGWCzK_Sav"));
+        categoryRVModalArrayList.add(new CategoryRVModal("Business", "https://drive.google.com/uc?export=view&id=1hI3ahSBHkpY7jzkIuRhu8V9GFJJ1uZXE"));
+        categoryRVModalArrayList.add(new CategoryRVModal("Entertainment", "https://drive.google.com/uc?export=view&id=1I9TENtoCtSTlTFBKw792JsD4vDKYVT6Z"));
+        categoryRVModalArrayList.add(new CategoryRVModal("Health", "https://drive.google.com/uc?export=view&id=1Ao0xwiETVbQveVKyUMLm8uIOn62m7O8T"));
         categoryRVAdapter.notifyDataSetChanged();
     }
 
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements CategoryRVAdapter
         articlesArrayList.clear();
         String categoryURL = "https://newsapi.org/v2/top-headlines?country=tr&category="+category+"&apiKey=5042b6d940884bcc9b107837bba7aaf3";
         System.out.println(category);
-        String url = "https://newsapi.org/v2/top-headlines?country=tr&excludeDomains=stackoverflow.com&sortBy=publishedAt&language=tr&apiKey=5042b6d940884bcc9b107837bba7aaf3";
+        String url = "https://newsapi.org/v2/top-headlines?country=tr&sortBy=publishedAt&language=tr&apiKey=5042b6d940884bcc9b107837bba7aaf3";
         String BASE_URL = "https://newsapi.org/";
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -102,7 +102,7 @@ public class MainActivity extends AppCompatActivity implements CategoryRVAdapter
     }
     @Override
     public void onCategoryClick(int position) {
-        String category = categoryRVModalArrayList.get(position).getCategory();
+        String category = categoryRVModalArrayList.get(position).getApiName();
         getNews(category);
     }
 }
